daf90c2d8fa7e63e146a62a82cd6abbcc81677d4
Renan Bueno <renanbuenov@hotmail.com>
2021-12-12 20:34:50 -0300

    Implementaçao de cache.

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package br.com.alura.mvc.mudi.repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +14,7 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    @Cacheable("books")
     List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("select p from Pedido p join p.user u where u.username = :username")
